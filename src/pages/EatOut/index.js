503313b3f67de51128f3f99cddeb082c2f6006fe
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-29 15:51:17 +0100

    :lipstick: change text color to be softer, add text-color variable and update across scss files, add alignment to headers

--- src/pages/EatOut/index.js
+++ src/pages/EatOut/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const EatOut = () => {
- 
+
     return (
         <>
             <header>
                 <h1>My Eating Adventures!</h1>
+                <h5>Cats are cute groom forever, stretch tongue and leave it slightly out, blep but reward the chosen human with a slow blink, and use lap as chair there's a forty year old lady there let us feast intently stare at the same spot. Chase laser small kitty warm kitty little balls of fur, poop in a handbag look delicious and drink the soapy mopping up water then puke giant foamy fur-balls</h5>
             </header>
         </>
     )
