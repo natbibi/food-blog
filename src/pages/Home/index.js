8b09d4e0a45fe2eed04c37c992cd9864de4752e2
natbibi <58271566+natbibi@users.noreply.github.com>
2021-06-12 23:47:43 +0100

    :sparkles: add logo / image to homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import stemoji from '../../Images/stemoji.png'
 
 const Home = () => {
  
     return (
         <>
-            <header>
+            <header id="homepage">
                 <h1>Hello World!</h1>
+                <img src={stemoji}/>
             </header>
         </>
     )
