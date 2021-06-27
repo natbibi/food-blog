f5bc4dc74035ae73c3e0c9f55f4b04b3f1b3b105
natbibi <58271566+natbibi@users.noreply.github.com>
2021-06-27 23:23:48 +0100

    :children_crossing: h1 logo / title links back to homepage on click

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Burger } from '../'
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <>
-        <h1 className="title">Ste's Food</h1>
+        <Link to="/" style={{ textDecoration: 'none'}}>
+        <h1 className="title" >Ste's Food</h1>
+        </Link>
         <nav role="navigation">
             <Burger />
         </nav>
