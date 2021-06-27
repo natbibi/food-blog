a579fa1a8f343117c95f4d1d33500e17c607f908
natbibi <58271566+natbibi@users.noreply.github.com>
2021-06-28 00:42:09 +0100

    :construction: :sparkles: render multiple images per post

--- src/components/EatOutItem/index.js
+++ src/components/EatOutItem/index.js
@@ -6,10 +6,17 @@ const EatOutItem = ({ postData, handleSelect }) => {
     const { id } = useParams();
 
     return (
-        <> 
+        <>
             <section className="item-container">
                 <div className="left">
                     <img style={{ width: "300px", height: "auto" }} src={postData.image} onClick={() => handleSelect(postData.id)} />
+
+                    <div className="more-images">
+                        <img style={{ width: "100px", height: "auto" }} src={postData.more_images[0]} />
+                        <img style={{ width: "100px", height: "auto" }} src={postData.more_images[0]} />
+                        <img style={{ width: "100px", height: "auto" }} src={postData.more_images[0]} />
+                    </div>
+                    
                 </div>
 
                 <div className="right">
@@ -21,7 +28,7 @@ const EatOutItem = ({ postData, handleSelect }) => {
                         <p>Atmosphere: {postData.atmosphere}⭐️ |</p>
                         <p>Service: {postData.service}⭐️</p>
                     </div>
-                    <p style={{ display: "flex", alignItems: "center"}}><IoLocationOutline /> {postData.restaurant_name}, {postData.location}</p>
+                    <p style={{ display: "flex", alignItems: "center" }}><IoLocationOutline /> {postData.restaurant_name}, {postData.location}</p>
                     <p style={{ textAlign: "justify" }}>{postData.description}</p>
                     {postData.recommend ? <p>Would I recommend: ✅ </p> : <p>Would I recommend: ❌</p>}
                 </div>
