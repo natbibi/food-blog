98ebfd46965c5d351297a3358e2572822a3a466e
natbibi <58271566+natbibi@users.noreply.github.com>
2021-06-27 21:09:55 +0100

    :lipstick: change UI - post title to overlay image on hover

--- src/components/EatOutList/index.js
+++ src/components/EatOutList/index.js
@@ -4,9 +4,8 @@ import { IoLocationOutline } from "react-icons/io5";
 const EatOutList = ({ postData, handleSelect }) => {
     return (
         <div className="post-container">
-
             <img src={postData.image} onClick={() => handleSelect(postData.id)} />
-            <h5 style={{ fontSize: "16px", textAlign: "center" }}><em>"{postData.title}"</em></h5>
+            <div className="centered hide"><h5 style={{ fontSize: "18px", textAlign: "center" }}><em>"{postData.title}"</em></h5></div>
             <p style={{ fontSize: "14px" }}>{postData.value + postData.taste + postData.atmosphere + postData.service} ⭐️ | {postData.restaurant_name}</p>
             <p style={{ display: "flex", alignItems: "center", fontSize: "12px", paddingBottom: "0.5rem" }}><IoLocationOutline />  {postData.location}</p>
         </div>
