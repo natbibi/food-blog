c7e378597922fd3ddeb482f7a05a794662ec566f
natbibi <58271566+natbibi@users.noreply.github.com>
2021-07-01 20:59:04 +0100

    :sparkles: add new component for eatin item - to render route data based on id

---
+++ src/components/EatInItem/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const EatInItem = ({ postData, handleSelect }) => {
+    const { id } = useParams()
+
+    return (
+        <>
+            <section className="item-container">
+               
+            </section>
+        </>
+    )
+}
+
+export default EatInItem
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as EatInList } from './EatInList' 
+export { default as EatInItem } from './EatInItem' 
 export { default as EatOutList } from './EatOutList' 
 export { default as EatOutItem } from './EatOutItem' 
