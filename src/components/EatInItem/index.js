78dc89e2e9b23fbf95fa709c3b123388b93d6b91
natbibi <58271566+natbibi@users.noreply.github.com>
2021-07-01 21:14:32 +0100

    :art: render fetched data for eatin

--- src/components/EatInItem/index.js
+++ src/components/EatInItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { FcCalendar } from "react-icons/fc";
 
 const EatInItem = ({ postData, handleSelect }) => {
     const { id } = useParams()
@@ -7,7 +8,15 @@ const EatInItem = ({ postData, handleSelect }) => {
     return (
         <>
             <section className="item-container">
-               
+            <div className="left">
+                    <img style={{ width: "300px", height: "auto" }} src={postData.image} onClick={() => handleSelect(postData.id)} />
+                </div>
+
+                <div className="right">
+                    <h3><em>"{postData.title}"</em></h3>
+                    <p style={{ display: "flex", alignItems: "center" }}><FcCalendar /> {postData.date}</p>
+                    <p style={{ textAlign: "justify" }}>{postData.description}</p>
+                </div>
             </section>
         </>
     )
