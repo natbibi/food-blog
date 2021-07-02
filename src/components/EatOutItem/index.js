3d5342688b792bfa6781a982db9193d07bf96604
natbibi <58271566+natbibi@users.noreply.github.com>
2021-07-02 18:12:43 +0100

    :lipstick: :children_crossing: improve UI and UX (hopefully) - better readibility, elements more spread out, main info in center with more spacing between

--- src/components/EatOutItem/index.js
+++ src/components/EatOutItem/index.js
@@ -9,29 +9,34 @@ const EatOutItem = ({ postData, handleSelect }) => {
         <>
             <section className="item-container">
                 <div className="left">
-                    <img style={{ width: "300px", height: "auto" }} src={postData.image} onClick={() => handleSelect(postData.id)} />
+                    <div className="post-container">
+                        <img style={{ width: "300px", height: "auto" }} src={postData.image} onClick={() => handleSelect(postData.id)} />
+                        <p style={{ fontSize: "14px" }}>{postData.value + postData.taste + postData.atmosphere + postData.service} ⭐️ | {postData.restaurant_name}</p>
+                        <p style={{ display: "flex", alignItems: "center", fontSize: "12px", paddingBottom: "0.5rem" }}><IoLocationOutline />  {postData.location}</p>
+                    </div>
                 </div>
 
                 <div className="right">
-                    <h3><em>"{postData.title}"</em></h3>
-                    <h6>{postData.date}</h6>
+                    <span style={{ fontSize: "12px", textAlign: "end" }}>{postData.date}</span>
+                    <h3 style={{ marginBlock: "1.5rem" }}><em>"{postData.title}" </em></h3>
+
                     <div className="ratings">
                         <p>Value: {postData.value} ⭐️ </p>
                         <p>Taste: {postData.taste} ⭐️ </p>
                         <p>Atmosphere: {postData.atmosphere} ⭐️ </p>
                         <p>Service: {postData.service} ⭐️</p>
                     </div>
-                    <p style={{ display: "flex", alignItems: "center" }}><IoLocationOutline /> {postData.restaurant_name}, {postData.location}</p>
+                    {/* <p style={{ display: "flex", alignItems: "center" }}><IoLocationOutline /> {postData.restaurant_name}, {postData.location}</p> */}
                     <p style={{ textAlign: "justify" }}>{postData.description}</p>
                     {postData.recommend ? <p>Would I recommend: ✅ </p> : <p>Would I recommend: ❌</p>}
-                
+
                     <div className="more-images">
                         <img src={postData.more_images[0]} />
                         <img src={postData.more_images[1]} />
                         <img src={postData.more_images[2]} />
                         <img src={postData.more_images[3]} />
                     </div>
-                
+
                 </div>
             </section>
         </>
